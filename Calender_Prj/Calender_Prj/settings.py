f2734b78dbc995365ed1fa88fc6bbdb210957577
JongukYang <didwhddnr123@naver.com>
2022-11-14 23:11:40 +0900

    Update settings.py

--- Calender_Prj/Calender_Prj/settings.py
+++ Calender_Prj/Calender_Prj/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s+a0)6p668xtb7g@^z^^c)m!c5e7m7f&+ly*dej(m6%fw&o9x7"
+# secret 키 관련한 모든 것은 secrets.json 에서 관리함
+secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+
+def get_secret(setting):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True  # 배포시에 바꾸기
 
 ALLOWED_HOSTS = []
 

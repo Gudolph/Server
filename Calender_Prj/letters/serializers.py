30325c57fef7c0f188e8e059ea208c6c95b81a88
sueun lee <1015_s@naver.com>
2022-11-19 09:26:37 +0900

    fix : calender get_username

--- Calender_Prj/letters/serializers.py
+++ Calender_Prj/letters/serializers.py
@@ -15,8 +15,12 @@ class LetterSerializer(serializers.ModelSerializer):
 # 캘린더
 class CalenderSerializer(serializers.ModelSerializer):
     letters = LetterSerializer(many=True, read_only=True)
-    owner = serializers.ReadOnlyField(source = 'user.username')
+    # owner = serializers.ReadOnlyField(source = 'user.username')
+    name = serializers.SerializerMethodField(method_name="get_username")
     
     class Meta:
         model = Calender
         fields = ("pk", "owner", "name", "letters")
+    
+    def get_username(self, obj):
+        return obj.owner.username
